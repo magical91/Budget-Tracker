939af95db550af96a82cb21e27dbf578de1fcb5c
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-06-25 16:22:33 -0500

    uploadTransations function was successful

--- public/js/idb.js
+++ public/js/idb.js
@@ -17,8 +17,7 @@ request.onsuccess = function (event) {
 
     // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-        // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
 
